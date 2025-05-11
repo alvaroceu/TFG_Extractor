36edeb3a711d56c0b1f9b5ecd6dce71e261d39c4
Alvaro <alvaro.sanchezmateos@usp.ceu.es>
2025-05-11 22:51:48 +0200

    test(bow): update tests to match the lastest preprocess(text) return format

--- tests/test_bow.py
+++ tests/test_bow.py
@@ -22,12 +22,13 @@ class TestPreprocessing(unittest.TestCase):
         self.assertIn("fly", lemmatized)
     
     def test_full_preprocess(self):
-        text = "The quick brown fox jumps over the lazy dog near the river, looking for something interesting to eat."
+        text = "The quick brown fox jumps over the lazy dogs near the river, looking for something interesting to eat."
         lemmatized = preprocess(text)
-        self.assertIn("fox", lemmatized)
-        self.assertIn("jump", lemmatized)
-        self.assertIn("look", lemmatized)
-        self.assertIn("eat", lemmatized)
-        self.assertNotIn("The", lemmatized)
-        self.assertNotIn("for", lemmatized)
-        self.assertNotIn("to", lemmatized)
+        tokens = lemmatized[0][1]
+        self.assertIn("fox", tokens)
+        self.assertIn("jump", tokens)
+        self.assertIn("look", tokens)
+        self.assertIn("eat", tokens)
+        self.assertNotIn("The", tokens)
+        self.assertNotIn("for", tokens)
+        self.assertNotIn("to", tokens)
