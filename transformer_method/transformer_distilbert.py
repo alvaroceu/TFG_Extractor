354ea2e81695b6d281e9b2187008cf7602554207
Alvaro <alvaro.sanchezmateos@usp.ceu.es>
2026-03-14 17:39:13 +0100

    feat(Transformer): Implement DistilBERT model as a faster pure QA version of BERT Large

---
+++ transformer_method/transformer_distilbert.py
@@ -0,0 +1,28 @@
+from transformers import pipeline
+from core.extractor_base import ExtractorBase
+from core.preprocessing import *
+
+model = pipeline('question-answering', model='twmkn9/distilbert-base-uncased-squad2', tokenizer='twmkn9/distilbert-base-uncased-squad2', device=0)
+
+class TransformerDistilBertExtractor(ExtractorBase):
+
+    def extract(self, text: str, questions: str):
+        """Extract relevant information from text for each column."""
+
+        results = {}
+
+        parsed_questions = parse_questions_embeddings(questions)
+
+        for key, question in parsed_questions.items():
+            
+            result = model(question=question,context=text,handle_impossible_answer=True)
+
+            if result['answer'].strip() == "":
+                best_answer = "A possible valid answer wasn't found"
+            else:
+                best_answer = result['answer']
+
+            results[key] = best_answer
+            print(f"{key}: score={result['score']:.3f} | answer={best_answer}")
+
+        return results
