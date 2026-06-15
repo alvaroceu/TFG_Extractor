4d3b1608c95c53dd7069cdbf600893dd9f2753c2
Alvaro <alvaro.sanchezmateos@usp.ceu.es>
2026-06-15 17:47:38 +0200

    feat(transformer_method): add BiL arquitectures

--- transformer_method/BiLBERT/arq_BiLBERT_Distil.py
+++ transformer_method/BiLBERT/arq_BiLBERT_Distil.py
@@ -4,22 +4,12 @@ from transformers import DistilBertModel
 
 class BiLBERTDistil(nn.Module):
     def __init__(self, model_name='twmkn9/distilbert-base-uncased-squad2', hidden_size=256):
-        # 1. Inicializamos la clase padre (Obligatorio en PyTorch)
+
+        # Download the DistilBERT model
         super(BiLBERTDistil, self).__init__()
-        
-        # 2. EL CUERPO: Cargamos el modelo base de DistilBERT. 
-        # Fíjate que usamos 'DistilBertModel' y no 'ForQuestionAnswering'. 
-        # Esto nos da el Transformer puro, sin la "cabeza" tonta original.
         self.distilbert = DistilBertModel.from_pretrained(model_name)
+        self.dropout = nn.Dropout(0.1)
         
-        # 3. CONGELACIÓN: Le decimos a PyTorch que no modifique los pesos de DistilBERT.
-        # ¿Por qué? Porque ya sabe inglés. Solo queremos enseñar a la nueva cabeza LSTM.
-        for param in self.distilbert.parameters():
-            param.requires_grad = False
-            
-        # 4. EL PUENTE: La red Bi-LSTM.
-        # Input: 768 (Porque DistilBERT escupe vectores de tamaño 768 por cada palabra)
-        # Output: 256 * 2 (Porque es bidireccional, lee hacia adelante y hacia atrás)
         self.lstm = nn.LSTM(
             input_size=768, 
             hidden_size=hidden_size, 
@@ -28,27 +18,27 @@ class BiLBERTDistil(nn.Module):
             bidirectional=True
         )
         
-        # 5. EL CLASIFICADOR FINAL: 
-        # Coge los 512 números que escupe el LSTM y los aplasta a solo 2 números por palabra:
-        # Probabilidad de [Inicio] y Probabilidad de [Fin]
-        self.qa_outputs = nn.Linear(hidden_size * 2, 2)
+        # Final clasifier layer
+        self.qa_outputs = nn.Linear(768 + (hidden_size * 2), 2)
 
     def forward(self, input_ids, attention_mask):
-        # A) El texto (números) pasa por el Transformer
+
+        # Through DistilBERT
         distilbert_output = self.distilbert(input_ids=input_ids, attention_mask=attention_mask)
-        
-        # B) Extraemos los "estados ocultos". Es una matriz con tamaño: (Batch, Num_Palabras, 768)
         sequence_output = distilbert_output[0]
+        sequence_output_dropped = self.dropout(sequence_output)
         
-        # C) Pasamos esa matriz por nuestra nueva capa Bi-LSTM
-        lstm_output, _ = self.lstm(sequence_output) # Se convierte en (Batch, Num_Palabras, 512)
+        # Through BiLSTM
+        lstm_output, _ = self.lstm(sequence_output_dropped) 
+        combined_output = torch.cat((sequence_output, lstm_output), dim=-1)
+        combined_output = self.dropout(combined_output)
+
+        # Through final linear layer
+        logits = self.qa_outputs(combined_output) 
         
-        # D) Pasamos el resultado por la capa lineal final
-        logits = self.qa_outputs(lstm_output) # Se convierte en (Batch, Num_Palabras, 2)
-        
-        # E) Separamos los 2 números finales. Uno es para el inicio, otro para el fin.
+        # Results: Start and End logits
         start_logits, end_logits = logits.split(1, dim=-1)
-        start_logits = start_logits.squeeze(-1) # Quitamos dimensiones vacías
+        start_logits = start_logits.squeeze(-1) 
         end_logits = end_logits.squeeze(-1)
         
         return start_logits, end_logits
--- transformer_method/BiLBERT/arq_BiLBERT_Large.py
+++ transformer_method/BiLBERT/arq_BiLBERT_Large.py
@@ -4,22 +4,12 @@ from transformers import BertModel
 
 class BiLBERTLarge(nn.Module):
     def __init__(self, model_name='deepset/bert-large-uncased-whole-word-masking-squad2', hidden_size=256):
-        # 1. Inicializamos la clase padre (Obligatorio en PyTorch)
+
+        # Download the BERTLarge model
         super(BiLBERTLarge, self).__init__()
-        
-        # 2. EL CUERPO: Cargamos el modelo base de DistilBERT. 
-        # Fíjate que usamos 'DistilBertModel' y no 'ForQuestionAnswering'. 
-        # Esto nos da el Transformer puro, sin la "cabeza" tonta original.
         self.bert = BertModel.from_pretrained(model_name)
-        
-        # 3. CONGELACIÓN: Le decimos a PyTorch que no modifique los pesos de DistilBERT.
-        # ¿Por qué? Porque ya sabe inglés. Solo queremos enseñar a la nueva cabeza LSTM.
-        for param in self.bert.parameters():
-            param.requires_grad = False
-            
-        # 4. EL PUENTE: La red Bi-LSTM.
-        # Input: 768 (Porque DistilBERT escupe vectores de tamaño 768 por cada palabra)
-        # Output: 256 * 2 (Porque es bidireccional, lee hacia adelante y hacia atrás)
+        self.dropout = nn.Dropout(0.1)
+
         self.lstm = nn.LSTM(
             input_size=1024, 
             hidden_size=hidden_size, 
@@ -28,27 +18,27 @@ class BiLBERTLarge(nn.Module):
             bidirectional=True
         )
         
-        # 5. EL CLASIFICADOR FINAL: 
-        # Coge los 512 números que escupe el LSTM y los aplasta a solo 2 números por palabra:
-        # Probabilidad de [Inicio] y Probabilidad de [Fin]
-        self.qa_outputs = nn.Linear(hidden_size * 2, 2)
+        # Final clasfier layer
+        self.qa_outputs = nn.Linear(1024 + (hidden_size * 2), 2)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        # A) El texto (números) pasa por el Transformer
+        
+        # Through BERTLarge
         bert_output = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        
-        # B) Extraemos los "estados ocultos". Es una matriz con tamaño: (Batch, Num_Palabras, 768)
         sequence_output = bert_output[0]
+        sequence_output_dropped = self.dropout(sequence_output)
+
+        # Through BiLSTM
+        lstm_output, _ = self.lstm(sequence_output_dropped) 
+        combined_output = torch.cat((sequence_output, lstm_output), dim=-1)
+        combined_output = self.dropout(combined_output)
+
+        # Through final linear layer
+        logits = self.qa_outputs(combined_output) 
         
-        # C) Pasamos esa matriz por nuestra nueva capa Bi-LSTM
-        lstm_output, _ = self.lstm(sequence_output) # Se convierte en (Batch, Num_Palabras, 512)
-        
-        # D) Pasamos el resultado por la capa lineal final
-        logits = self.qa_outputs(lstm_output) # Se convierte en (Batch, Num_Palabras, 2)
-        
-        # E) Separamos los 2 números finales. Uno es para el inicio, otro para el fin.
+        # Results: Start and End logits
         start_logits, end_logits = logits.split(1, dim=-1)
-        start_logits = start_logits.squeeze(-1) # Quitamos dimensiones vacías
+        start_logits = start_logits.squeeze(-1) 
         end_logits = end_logits.squeeze(-1)
         
         return start_logits, end_logits
